260aa2b894e639a9b737f86b9611b624cc4c9fa7
susan31213 <susan31213@gmail.com>
2016-06-04 00:48:13 +0800

    lab8

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	try{
+		throw 3.0;
+	} catch (double) {
+		cout << "exception caught" << endl;
+	}
+	cout << " I love C++" << endl;
+	return 0;
+}
